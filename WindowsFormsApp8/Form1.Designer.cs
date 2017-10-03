a3e00715c88552c3934398e229ed6fb3f1e08948
xiefengan <30953779+xiefengan@users.noreply.github.com>
2017-10-03 23:48:14 +0800

    The project of class in STUST
    Number string was handled and was saved in array of string.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "特別獎";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "特獎";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "頭獎";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(199, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Loading...";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(199, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Loading...";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(199, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Loading...";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(12, 195);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "增開六獎";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(199, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 19);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Loading...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(129, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "讀取中，請耐心等待 謝謝><";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 65);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(179, 226);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 36);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(176, 274);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(169, 16);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Data Loading Please Wait";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(498, 406);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp8
+{
+
+    public partial class Form1 : Form
+    {
+        Thread t1;
+        string[] number = new string[8];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string str;
+        private void getNumber()
+        {
+            WebRequest myrequest =
+            WebRequest.Create(@"http://invoice.etax.nat.gov.tw/");
+            myrequest.Method = "GET";
+            WebResponse respon = myrequest.GetResponse();
+            myrequest.Timeout = 1000;
+            StreamReader sr = new StreamReader(respon.GetResponseStream());
+            str = sr.ReadToEnd();
+            sr.Close();
+            respon.Close();
+        }
+        private void HandlingNuber()
+        {
+            int StrLocation = str.IndexOf("t18Red\">");
+
+            //搜尋特別獎
+            number[0] = str.Substring(StrLocation + 8, 8);
+            label5.Text = number[0];
+
+            //搜尋特獎
+            StrLocation = str.IndexOf("t18Red\">", StrLocation + 10);
+            number[1] = str.Substring(StrLocation + 8, 8);
+            label6.Text = number[1];
+
+            //搜尋頭獎
+            StrLocation = str.IndexOf("t18Red\">", StrLocation + 10);
+            StrLocation += 8;
+            label7.Text = "";
+            for (int i = 0; i < 3; i++)
+            {
+                number[i + 2]= str.Substring(StrLocation, 8);
+                StrLocation+=9;
+                if (i > 0)
+                    label7.Text += "　" + number[i + 2];
+                else
+                    label7.Text += number[i + 2];
+            }
+
+            //搜尋增開六獎
+            StrLocation = str.IndexOf("t18Red\">", StrLocation + 10);
+            StrLocation += 8;
+            label9.Text = "";
+            for (int i = 0; i <3; i++)
+            {
+                number[i + 5] = str.Substring(StrLocation, 3);
+                StrLocation+=4;
+                if (i > 0)
+                    label9.Text += "　" + number[i + 5];
+                else
+                    label9.Text += number[i + 5];
+            }
+
+            //搜尋兌獎發票月份
+            StrLocation = str.IndexOf("<h2>106");
+
+            string one = str.Substring(StrLocation + 4, 10);
+            label1.Text = "統一發票兌獎:" + one;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            button1.Visible = false;
+            button1.Enabled = false;
+            button1.Text = "Loading data...";
+           t1 = new Thread(getNumber);
+            t1.Start();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (t1.IsAlive == false)
+            {
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                label10.Text = "請輸入想要查詢的發票號碼";
+                button1.Text = "查詢發票號碼";
+                HandlingNuber();
+                timer1.Enabled = false;
+                button1.Visible = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string num = textBox1.Text;
+            int n;
+            if (num == "")
+            {
+                label10.Text= "請輸入想要查詢的發票號碼";
+            }
+            else if ((Encoding.Default.GetBytes(num).Length != 8) || (!int.TryParse(num, out n)))
+            {
+                label10.Text = "請輸入正確的發票號碼";
+            }
+            else
+            {
+                label10.Text = "MeSoHandSoMe!";
+            }
+        }
+    }
+}
